c205a9dd8492942b448a449fdde43fe99d841982
Nicola Solazzo <solazzo.nicola@gmail.com>
2025-06-05 20:36:28 +0200

    create pixel map

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -5,6 +5,7 @@ defmodule Identicon do
     |> pick_color()
     |> build_grid()
     |> filter_odd_sqares()
+    |> build_pixel_map()
   end
 
   def build_grid(%Identicon.Image{hex: hex} = image) do
@@ -16,6 +17,20 @@ defmodule Identicon do
       |> Enum.with_index()
 
     %Identicon.Image{image | grid: grid}
+  end
+
+  def build_pixel_map(%Identicon.Image{grid: grid} = image) do
+    pixel_map = Enum.map(grid, fn {_code, index} ->
+      horizontal = rem(index,5) * 50
+      vertical = div(index, 5) * 50
+
+      top_left = {horizontal, vertical}
+      bottom_right = {horizontal + 50, vertical + 50}
+
+      {top_left, bottom_right}
+    end)
+
+    %Identicon.Image{image | pixel_map: pixel_map}
   end
 
   def filter_odd_sqares(%Identicon.Image{grid: grid} = image) do
--- identicon/lib/image.ex
+++ identicon/lib/image.ex
@@ -2,6 +2,7 @@ defmodule Identicon.Image do
     defstruct [
       hex: nil,
       color: nil,
-      grid: nil
+      grid: nil,
+      pixel_map: nil
   ]
 end
